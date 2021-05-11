5f24af46a1fc354b42c30b37bde36b0ac1cf1201
Rosalee Elting <relting@uw.edu>
2021-05-11 15:48:14 -0400

    Code that is mostly working (outputting a table), but is only subsetting from Clade, not museum.

---
+++ spread_wing_finder/app.R
@@ -0,0 +1,27 @@
+# Attach necessary packages
+library(tidyverse)
+library(shiny)
+library(shinythemes)
+
+wings <- read_csv("spread_wings_full_directory_cleaned.csv")
+
+
+# Create the user interface:
+ui <- fluidPage(theme= shinytheme("slate"),
+  sidebarLayout(
+    sidebarPanel(selectInput("museum", "Choose your museum:", 
+                            choices = unique(wings$institution)),
+    
+                 radioButtons("clade", "Select a clade:", 
+                              choices = unique(wings$clade))),
+  mainPanel(tableOutput("wingsdata"))))
+# Create the server function:
+server <- (function(input, output) {
+ output$wingsdata <- renderTable({
+   museumFilter <- subset(wings, wings$institutioncode== input$museum, c("institution", "clade", "genus", "specificepithet", "infraspecificepithet", "vernacularname", "sex", "catalognumber"))
+   cladeFilter <- subset(wings, wings$clade == input$clade, c("institution", "clade", "genus", "specificepithet", "infraspecificepithet", "vernacularname", "sex", "catalognumber"))
+ })
+ })
+ 
+# Combine them into an app:
+shinyApp(ui = ui, server = server)
