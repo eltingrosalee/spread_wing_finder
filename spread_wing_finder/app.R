764695b9434907e03472b96b2f2e69520d58a8fa
Rosalee Elting <relting@uw.edu>
2021-05-21 12:47:41 -0400

    adding R code to GH.

--- spread_wing_finder/app.R
+++ spread_wing_finder/app.R
@@ -10,19 +10,14 @@ wings <- read_csv("spread_wings_full_directory_cleaned.csv")
 ui <- fluidPage(theme= shinytheme("slate"),
   sidebarLayout(
     sidebarPanel(selectInput("museum", "Choose your museum:", 
-                            choices = unique(wings$institution)),
-    
-                 radioButtons("clade", "Select a clade:", 
-                              choices = unique(wings$clade))),
+                            choices = unique(wings$institution))),
   mainPanel(tableOutput("wingsdata")),
   fluid=TRUE))
 # Create the server function:
 server <- (function(input, output) {
  output$wingsdata <- renderTable({
    museumFilter <- subset(wings, wings$institution== input$museum, c("institution", "clade", "genus", "specificepithet", "infraspecificepithet", "vernacularname", "sex", "catalognumber"))
-   cladeFilter <- subset(wings, wings$clade == input$clade, c("institution", "clade", "genus", "specificepithet", "infraspecificepithet", "vernacularname", "sex", "catalognumber"))
- })
- })
+   })
  
 # Combine them into an app:
 shinyApp(ui = ui, server = server)
