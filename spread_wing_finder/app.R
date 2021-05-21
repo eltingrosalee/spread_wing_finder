eaa2edddbaa76e0a0d3d72b26bbb828faa9d91bf
Rosalee Elting <relting@uw.edu>
2021-05-21 13:16:12 -0400

    updated code where both filters work!!!!

--- spread_wing_finder/app.R
+++ spread_wing_finder/app.R
@@ -8,16 +8,19 @@ wings <- read_csv("spread_wings_full_directory_cleaned.csv")
 
 # Create the user interface:
 ui <- fluidPage(theme= shinytheme("slate"),
-  sidebarLayout(
-    sidebarPanel(selectInput("museum", "Choose your museum:", 
-                            choices = unique(wings$institution))),
-  mainPanel(tableOutput("wingsdata")),
-  fluid=TRUE))
+                sidebarLayout(
+                  sidebarPanel(selectInput("museum", "Choose your museum:", 
+                                           choices = unique(wings$institution)),
+                               radioButtons("clade", "Select a clade:", 
+                                            choices = unique(wings$clade))),
+                  mainPanel(tableOutput("wingsdata")),
+                  fluid=TRUE))
 # Create the server function:
 server <- (function(input, output) {
- output$wingsdata <- renderTable({
-   museumFilter <- subset(wings, wings$institution== input$museum, c("institution", "clade", "genus", "specificepithet", "infraspecificepithet", "vernacularname", "sex", "catalognumber"))
-   })
- 
+  output$wingsdata <- renderTable({
+    museumFilter <- subset(wings, wings$institution== input$museum & wings$clade == input$clade, c("institution", "clade", "genus", "specificepithet", "infraspecificepithet", "vernacularname", "sex", "catalognumber"))
+  })
+})
+
 # Combine them into an app:
 shinyApp(ui = ui, server = server)
