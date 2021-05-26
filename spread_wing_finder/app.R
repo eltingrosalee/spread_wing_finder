a79d66be3534913610d1f70f50baccb8a6ab10e8
Rosalee Elting <relting@uw.edu>
2021-05-26 10:17:09 -0400

    working code

--- spread_wing_finder/app.R
+++ spread_wing_finder/app.R
@@ -3,21 +3,26 @@ library(tidyverse)
 library(shiny)
 library(shinythemes)
 library(rsconnect)
-rsconnect::setAccountInfo(name='eltingrosalee', token='C4A30BF752FCCC69631C3EB735764E4B', secret='U9Cvo7bLdpQwe2IxztLyk6NxI/+bt/cZA9M8u++V')
+library(shinyWidgets)
+#rsconnect for publishing the app 
 
-wings <- read_csv("spread_wings_full_directory_cleaned.csv")
+rsconnect::setAccountInfo(name='eltingrosalee', token='C4A30BF752FCCC69631C3EB735764E4B', secret='U9Cvo7bLdpQwe2IxztLyk6NxI/+bt/cZA9M8u++V')
+#inputting data 
+wings_csv <- read_csv("spread_wings_full_directory_cleaned.csv")
+#saving as a data frame
+wings <- data.frame(wings_csv)
 
 
 # Create the user interface:
 ui <- fluidPage(theme= shinytheme("slate"),
                 sidebarLayout(
-                  sidebarPanel(selectInput("museum", "Choose your museum:", 
-                                           choices = unique(wings$institution)),
-                               radioButtons("clade", "Select a clade:", 
-                                            choices = unique(wings$clade)),
-                               selectInput("sex", "Select sex of interest:",
-                                           choices = unique(wings$sex)),
-                               h6("Powered by:",height =20),
+                  sidebarPanel(pickerInput("museum", "Choose your museum:", 
+                                           choices = unique(wings$institution),options = list(`actions-box` = TRUE),multiple = T),
+                               awesomeCheckboxGroup("clade", "Clade:", 
+                                            choices = unique(wings$clade), width =1, status= "info"),
+                               pickerInput("sex", "Select sex of interest:",
+                                           choices = unique(wings$sex),options = list(`actions-box` = TRUE),multiple = T),
+                               h6("Powered by:",height =20), 
                                tags$img(src='be_logo.png', align = "left", height=200, width=200)),
                   mainPanel(tableOutput("wingsdata"),
                   fluid=TRUE)))
