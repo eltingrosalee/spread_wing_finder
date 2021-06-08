14835a215ffbc85adf74dd399f5a0be18704aeab
Rosalee Elting <relting@uw.edu>
2021-06-08 12:16:59 -0400

    added titles to the app, formatted for a link in the "powered  by" section to later add the lab's webpage. Still would like to change oclumn names, but want to not have to recode, only for display.

--- spread_wing_finder/app.R
+++ spread_wing_finder/app.R
@@ -7,7 +7,7 @@ library(shinyWidgets)
 library(gitcreds)
 #rsconnect for publishing the app 
 rsconnect::setAccountInfo(name='eltingrosalee', token='C4A30BF752FCCC69631C3EB735764E4B', secret='U9Cvo7bLdpQwe2IxztLyk6NxI/+bt/cZA9M8u++V')
-#setting up GH token 
+#setting up GH token using git creds package 
 
 #inputting data 
 wings_csv <- read_csv("spread_wings_full_directory_cleaned.csv")
@@ -17,6 +17,7 @@ wings <- data.frame(wings_csv)
 
 # Create the user interface:
 ui <- fluidPage(theme= shinytheme("slate"),
+                titlePanel(h1(strong("Spread Wing Finder"), style = "font-size:40px;")),
                 sidebarLayout(
                   sidebarPanel(pickerInput("museum", "Choose your museum:", 
                                            choices = unique(wings$institution),options = list(`actions-box` = TRUE),multiple = T, selected = "University of Washington Burke Museum"),
@@ -24,10 +25,10 @@ ui <- fluidPage(theme= shinytheme("slate"),
                                             choices = unique(wings$clade), width =1, status= "info"),
                                pickerInput("sex", "Select sex of interest:",
                                            choices = unique(wings$sex), selected = "male", options = list(`actions-box` = TRUE),multiple = T),
-                               h6("Powered by:",height =20), 
+                               h6(a(href="https://rosaleeelting.wixsite.com/mysite","Powered by:",height =20)), 
                                tags$img(src='be_logo.png', align = "left", height=200, width=200)),
-                  mainPanel(tableOutput("wingsdata")),
-                  fluid=TRUE))
+                  mainPanel(titlePanel(h1("Wings Available:", align = "center",style = "font-size:20px;")),tableOutput("wingsdata"),
+                  fluid=TRUE)))
 # Create the server function:
 server <- (function(input, output) {
   output$wingsdata <- renderTable({ subset(wings, wings$institution== input$museum & wings$clade == input$clade & input$sex == wings$sex, 
